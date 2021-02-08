7bc44c523c2c1059675cac550095f8ff98cda8ee
LauraQuantrille <lquantrille@gmail.com>
2021-02-08 15:36:04 -0500

    team work movie js

---
+++ main.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('.the-form')
+let movieList = document.querySelector('.the-movie-list')
+let movie = document.querySelector('.the-movie').value
+
+
+form.addEventListener('click', e => {
+    e.preventDefault();
+})
+
+
+const addMovie = document.querySelector('.the-post-button')
+    addMovie.addEventListener('click', createMovie())
+     
+
+
+function createMovie() {
+    let movie = document.querySelector('.the-movie').value
+    renderMovie(movie)
+    console.log('movie?', movie)
+}
+
+
+
+    function renderMovie(movie) {
+    let movieEl = document.createElement('li')
+    movieEl.classname = "list-item";
+    movieEl.innerHTML = `${movie}`
+    movieList.appendChild(movieEl)
+    
+}
+
+ function listMovies()   
+    
