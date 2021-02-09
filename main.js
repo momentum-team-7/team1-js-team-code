3e53e09953bcdc51f2a2e83cb70af3c4a806a53a
Tip Powell <powelltip@gmail.com>
2021-02-09 11:09:16 -0500

    commit for shift change

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('.the-form')
 let movieList = document.querySelector('.the-movie-list')
 let movie = document.querySelector('.the-movie').value
-
+const url = 'http://localhost:3000/movies'
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -10,25 +10,35 @@ form.addEventListener('submit', e => {
 
 const addMovie = document.querySelector('.the-post-button')
     addMovie.addEventListener('click', createMovie)
-     
+
 
 
 function createMovie() {
     console.log('create movie called')
     let movie = document.querySelector('input').value
-    //renderMovie(movie)
+    renderMovie(movie)
     console.log('movie?', movie)
     
 }
 
 
 
-//     function renderMovie(movie) {
-//     let movieEl = document.createElement('li')
-//     movieEl.classname = "list-item";
-//     movieEl.innerHTML = `${movie}`
-//     movieList.appendChild(movieEl)
+function renderMovie(movie) {
+    let movieEl = document.createElement('li')
+    movieEl.classname = "list-item";
+    movieEl.innerHTML = `${movie}`
+    movieList.appendChild(movieEl)
     
-// }
+}
 
- //function listMovies() {};
+    function listMovies() {
+        console.log('confirm listMovies function ran'), 
+        fetch(url)
+        .then (resp =>
+        resp.json()    )
+        .then(data => {
+            // for item **need to write for loop for each object in data
+        })
+    };
+
+    listMovies()
